8d4d1b95376828651b8ef1f1b1a3f74005789317
bdsilva16 <bd6866190@gmail.com>
2023-07-10 13:37:40 -0300

    estamos tentando

--- src/components/mapComp.tsx
+++ src/components/mapComp.tsx
@@ -2,7 +2,10 @@ import React from "react";
 import { TouchableOpacity, Text, StyleSheet, View } from "react-native";
 import MapView, { Marker } from "react-native-maps";
 import { Feather } from '@expo/vector-icons';
-import TouristSpotEntity from '../entities/tourist_spot_entity'
+import TouristSpotEntity from "../entities/tourist_spot_entity";
+import {Image} from "expo-image"
+
+
 
 export default function MapComp({ navigation }) {
   return (
@@ -13,8 +16,14 @@ export default function MapComp({ navigation }) {
             key={index}
             coordinate={{ latitude: ponto.latitude, longitude: ponto.longitude }}
             title={ponto.nome}
-            
-          />
+          >
+            <View style={styles.markerContainer}>
+              <Image
+                source={ponto.imagem}
+                style={styles.markerImage}
+              />
+            </View>
+            </Marker>
         ))}
       </MapView>
       <TouchableOpacity
@@ -39,7 +48,7 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   button: {
-    bottom: 50,
+    bottom: 70,
     right: 20,
     backgroundColor: "transparent",
     width: 70,
@@ -54,5 +63,18 @@ const styles = StyleSheet.create({
   buttonText: {
     fontSize: 15,
     color: '#fff',
+  },
+  markerContainer: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    overflow: 'hidden',
+  },
+  markerImage: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
+    elevation:10,
+    shadowColor:'black'
   },
 });
